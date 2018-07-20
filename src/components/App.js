3e39f839ad8168c6e2929cf62d288b1e7aeba1a6
Mustafa Temel <mustafa.temel@argos.co.uk>
2018-07-20 15:03:18 +0100

    Implemented navigation bar version 1.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Nav from './Nav'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      'hamburgerToggle': false
+    }
+    this.handleHamburgerToggle = this.handleHamburgerToggle.bind(this)
+  }
+
+  handleHamburgerToggle () {
+    this.setState((prevState) => ({
+      'hamburgerToggle': !prevState.hamburgerToggle
+    }))
+  }
+
   render () {
+    const { hamburgerToggle } = this.state
     return (
-      <div id='app'></div>
+      <div id='app'>
+        <Nav hamburgerToggle={hamburgerToggle} handleHamburgerToggle={this.handleHamburgerToggle} />
+      </div>
     )
   }
 }
