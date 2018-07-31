55c021b2f0e1c81b2bbcc085e81bbb56ee72f508
Mustafa Temel <mustafa.temel@argos.co.uk>
2018-07-31 01:12:57 +0100

    Tweaks to portfolio data.

--- src/components/portfolio-data.js
+++ src/components/portfolio-data.js
@@ -14,71 +14,71 @@ const portfolioData = [
     'title': 'Argos',
     'url': '//www.argos.co.uk',
     'logo': argos,
-    'role': 'Analytics implementation, A/B and multivariate testing',
-    'tech': ['Adobe Analytics', 'Adobe DTM', 'Tealium', 'HTML', 'CSS', 'JavaScript']
+    'role': ['Analytics implementation', 'A/B and multivariate testing'],
+    'tech': ['HTML', 'CSS', 'JavaScript', 'React'] /* Adobe Analytics, Adobe DTM, Tealium */
   },
   {
     'title': 'Belsize Bathrooms',
     'url': '//www.belsizebathrooms.co.uk',
     'logo': belsizeBathrooms,
-    'role': 'Design, front-end + back-end development',
+    'role': ['Design', 'Front-end development', 'Back-end development'],
     'tech': ['HTML', 'CSS', 'JavaScript', 'PHP']
   },
   {
     'title': 'British Airways',
     'url': '//www.britishairways.com/travel/home/public/en_gb', /* Redirects to https://www.britishairways.com/en-gb/home#/ */
     'logo': britishAirways,
-    'role': 'A/B and multivariate testing',
+    'role': ['A/B and multivariate testing'],
     'tech': ['HTML', 'CSS', 'JavaScript']
   },
   {
     'title': 'Ebuyer',
     'url': '//www.ebuyer.com',
     'logo': ebuyer,
-    'role': 'A/B and multivariate testing',
+    'role': ['A/B and multivariate testing'],
     'tech': ['HTML', 'CSS', 'JavaScript']
   },
   {
     'title': 'Energy Helpline',
     'url': '//www.energyhelpline.com', /* Redirects to https://www.energyhelpline.com/fri/ */
     'logo': energyHelpline,
-    'role': 'A/B and multivariate testing',
+    'role': ['A/B and multivariate testing'],
     'tech': ['HTML', 'CSS', 'JavaScript', 'Angular']
   },
   {
     'title': 'Heathrow Express',
     'url': '//www.heathrowexpress.com',
     'logo': heathrowExpress,
-    'role': 'A/B and multivariate testing',
+    'role': ['A/B and multivariate testing'],
     'tech': ['HTML', 'CSS', 'JavaScript']
   },
   {
     'title': 'John Lewis',
     'url': '//www.johnlewis.com',
     'logo': johnLewis,
-    'role': 'A/B and multivariate testing',
+    'role': ['A/B and multivariate testing'],
     'tech': ['HTML', 'CSS', 'JavaScript']
   },
   {
     'title': 'National Rail Enquiries',
     'url': '//www.nationalrail.co.uk',
     'logo': nationalRailEnquiries,
-    'role': 'A/B and multivariate testing, front-end development',
+    'role': ['A/B and multivariate testing', 'Front-end development'],
     'tech': ['HTML', 'CSS', 'JavaScript']
   },
   {
     'title': 'Thomas Cook',
     'url': '//www.thomascook.com',
     'logo': thomasCook,
-    'role': 'A/B and multivariate testing',
+    'role': ['A/B and multivariate testing'],
     'tech': ['HTML', 'CSS', 'JavaScript', 'Angular']
   },
   {
     'title': 'tint',
     'url': '//play.google.com/store/apps/details?id=com.temelm.tint',
     'logo': tint,
-    'role': 'Design, Hybrid mobile app development',
-    'tech': ['HTML', 'CSS', 'JavaScript', 'Adobe PhoneGap']
+    'role': ['Design', 'Hybrid mobile app development'],
+    'tech': ['HTML', 'CSS', 'JavaScript'] /* Adobe PhoneGap */
   }
 ]
 
