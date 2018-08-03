05dbb2cf967754322d0c47f3fa992d4545f11e98
Mustafa Temel <mustafa.temel@argos.co.uk>
2018-08-03 11:21:28 +0100

    Added PortfolioCard component and its styling.

---
+++ src/components/PortfolioCard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Tech from './Tech'
+
+const PortfolioCard = (props) => {
+  return (
+    <div className='portfolio-card'>
+      <div className='portfolio-card-logo'>
+        <a href={props.url} target='_blank'>
+          <img src={props.logo} alt={props.title} />
+        </a>
+      </div>
+      <div className='portfolio-card-body'>
+        <div className='portfolio-card-title'>
+          <h2>{props.title}</h2>
+        </div>
+        <div className='portfolio-card-details'>
+          <div className='portfolio-card-role'>
+            <h4>Role</h4>
+            <ul>
+              {props.role.map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
+          </div>
+          <div className='portfolio-card-tech'>
+            <h4>Tech</h4>
+            <div className='tech-stack'>
+              {props.tech.map((item, index) => <Tech key={index} name={item} />)}
+            </div>
+          </div>
+        </div>
+        <div className='portfolio-card-cta'>
+          <a href={props.url} target='_blank'>Visit site</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PortfolioCard
