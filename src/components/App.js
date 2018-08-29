c80d985066fa5a0e0523fe0c1fa277b20baef6a1
Mustafa Temel <mustafa.temel@argos.co.uk>
2018-08-29 16:45:03 +0100

    Added notification messages.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Nav from './Nav'
 import Hello from './Hello'
 import Portfolio from './Portfolio'
 import Contact from './Contact'
+import Notification from './Notification'
 
 import portfolioData from './portfolio-data'
 
@@ -10,9 +11,14 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      'hamburgerToggle': false
+      'hamburgerToggle': false,
+      'contactFormSubmitDisabled': false,
+      'displayNotification': false,
+      'notificationType': '',
+      'notificationMessage': ''
     }
     this.handleHamburgerToggle = this.handleHamburgerToggle.bind(this)
+    this.handleContactFormSubmit = this.handleContactFormSubmit.bind(this)
   }
 
   handleHamburgerToggle (event) {
@@ -22,14 +28,59 @@ class App extends React.Component {
     }))
   }
 
+  handleContactFormSubmit (event) {
+    event.preventDefault()
+    this.setState({
+      'contactFormSubmitDisabled': true
+    })
+    const contactForm = event.target
+    fetch('mail.php', {
+      'method': 'post',
+      'body': new FormData(contactForm)
+    }).then((response) => response.text()).then((text) => {
+      if (text === 'success') {
+        contactForm.reset()
+        this.setState({
+          'displayNotification': true,
+          'notificationType': 'success',
+          'notificationMessage': 'Sent.'
+        })
+      } else {
+        this.setState({
+          'displayNotification': true,
+          'notificationType': 'error',
+          'notificationMessage': 'Your message was not sent. Please try again.'
+        })
+      }
+      setTimeout(() => {
+        this.setState({
+          'contactFormSubmitDisabled': false
+        })
+      }, 1000)
+      setTimeout(() => {
+        this.setState({
+          'displayNotification': false,
+          'notificationType': '',
+          'notificationMessage': ''
+        })
+      }, 8000)
+    }).catch((error) => {
+      console.error(error)
+    })
+  }
+
   render () {
-    const { hamburgerToggle } = this.state
+    const { hamburgerToggle, contactFormSubmitDisabled, displayNotification, notificationType, notificationMessage } = this.state
     return (
       <div id='app'>
         <Nav hamburgerToggle={hamburgerToggle} handleHamburgerToggle={this.handleHamburgerToggle} />
         <Hello />
         <Portfolio portfolioData={portfolioData} />
-        <Contact />
+        <Contact
+          contactFormSubmitDisabled={contactFormSubmitDisabled}
+          handleContactFormSubmit={this.handleContactFormSubmit}
+        />
+        {displayNotification && <Notification type={notificationType} message={notificationMessage} />}
       </div>
     )
   }
