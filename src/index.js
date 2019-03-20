8326e13fff970f43647bd96bad80c688672a6c92
Mustafa Temel <mustafatemel90@gmail.com>
2019-03-20 10:23:49 +0000

    Fixed minor ESLint errors.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
 import './styles/index.scss'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { faAngular, faCss3Alt, faGithub, faHtml5, faLinkedinIn, faJs, faPhp, faReact } from '@fortawesome/free-brands-svg-icons'
+import {
+  faAngular, faCss3Alt, faGithub, faHtml5, faLinkedinIn, faJs, faPhp, faReact
+} from '@fortawesome/free-brands-svg-icons'
+import App from './components/App'
 
 library.add(faBars, faAngular, faCss3Alt, faGithub, faHtml5, faLinkedinIn, faJs, faPhp, faReact)
 
