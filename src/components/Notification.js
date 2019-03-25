8a28a086cc955498bbf46a61e57ca3de58edeb1d
Mustafa Temel <mustafatemel90@gmail.com>
2019-03-25 14:54:42 +0000

    Added notification dismiss link.

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Notification = (props) => {
-  const { type, message } = props
+  const { type, message, handleNotificationDismiss } = props
   return (
     <div className={`notification notification-${type}`}>
       <div className='wrapper'>
         {message}
+        {' '}
+        <a href='#' onClick={handleNotificationDismiss}>Dismiss</a>
       </div>
     </div>
   )
