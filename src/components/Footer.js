c260f5a7f8402d34e93f3d053557d09d93bd4ee9
Mustafa Temel <mustafatemel90@gmail.com>
2020-08-25 16:36:52 +0300

    Add Footer component.

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Footer = () => (
+  <section id='footer'>
+    <div className='wrapper'>
+      <div className='social-stack'>
+        <div className='social social-github'>
+          <a href='//github.com/temelm' target='blank' rel='noopener'>
+            <FontAwesomeIcon icon={['fab', 'github']} fixedWidth size='lg' spin />
+          </a>
+        </div>
+        <div className='social social-linkedin'>
+          <a href='//www.linkedin.com/in/mustafa-temel-b3908b2a' target='blank' rel='noopener'>
+            <FontAwesomeIcon icon={['fab', 'linkedin-in']} fixedWidth size='lg' spin />
+          </a>
+        </div>
+      </div>
+      <div className='copyright'>
+        <span role='img' aria-label='Copyright'>©️</span>
+        {' '}
+        2020 Mustafa Temel
+      </div>
+    </div>
+  </section>
+)
+
+export default Footer
