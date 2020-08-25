b7efb7ef47d73311d0b19b5322e0c1a1f885d7ab
Mustafa Temel <mustafatemel90@gmail.com>
2020-08-25 14:24:05 +0300

    Remove social and copyright from Contact component.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import ContactForm from './ContactForm'
 
 const Contact = (props) => {
@@ -12,25 +11,6 @@ const Contact = (props) => {
           contactFormSubmitDisabled={contactFormSubmitDisabled}
           handleContactFormSubmit={handleContactFormSubmit}
         />
-        <div className='contact-social'>
-          <div className='social-stack'>
-            <div className='social social-github'>
-              <a href='//github.com/temelm' target='blank' rel='noopener'>
-                <FontAwesomeIcon icon={['fab', 'github']} fixedWidth size='lg' spin />
-              </a>
-            </div>
-            <div className='social social-linkedin'>
-              <a href='//www.linkedin.com/in/mustafa-temel-b3908b2a' target='blank' rel='noopener'>
-                <FontAwesomeIcon icon={['fab', 'linkedin-in']} fixedWidth size='lg' spin />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className='copyright'>
-          <span role='img' aria-label='Copyright'>©️</span>
-          {' '}
-          2020 Mustafa Temel
-        </div>
       </div>
     </section>
   )
