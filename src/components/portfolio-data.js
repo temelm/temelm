54022d1c2596ea189f1ec25e9b78232c2f5b38a5
Mustafa Temel <mustafatemel90@gmail.com>
2020-08-25 16:41:45 +0300

    Re-order portfolio cards. Replace isContract with subTitle. Add Tu Clothing and Tealman. Add date field for each card.

--- src/components/portfolio-data.js
+++ src/components/portfolio-data.js
@@ -1,5 +1,4 @@
 import argos from '../images/argos.jpg'
-import belsizeBathrooms from '../images/belsize-bathrooms.png'
 import britishAirways from '../images/british-airways.jpg'
 import ebuyer from '../images/ebuyer.jpg'
 import energyHelpline from '../images/energy-helpline.jpg'
@@ -7,88 +6,103 @@ import eurostar from '../images/eurostar.jpg'
 import heathrowExpress from '../images/heathrow-express.jpg'
 import johnLewis from '../images/john-lewis.gif'
 import nationalRailEnquiries from '../images/national-rail-enquiries.jpg'
-import thomasCook from '../images/thomas-cook.jpg'
+import tealman from '../images/tealman.png'
 import tint from '../images/tint.png'
+import tuClothing from '../images/tu-clothing.jpeg'
 
 const portfolioData = [
+  {
+    title: 'Tu Clothing',
+    subTitle: 'Contract',
+    url: '//tuclothing.sainsburys.co.uk',
+    logo: tuClothing,
+    date: 'May 2020 - Present',
+    role: ['Digital Analytics Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'], /* @todo: REVIEW */
+    isPrimary: true
+  },
+  {
+    title: 'Eurostar',
+    subTitle: 'Contract',
+    url: '//www.eurostar.com',
+    logo: eurostar,
+    date: 'November 2018 - April 2020',
+    role: ['Digital Analytics Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'] /* @todo: REVIEW */
+  },
   {
     title: 'Argos',
     url: '//www.argos.co.uk',
     logo: argos,
-    role: ['Analytics implementation', 'A/B and multivariate testing'],
-    tech: ['HTML', 'CSS', 'JavaScript', 'React'] /* Adobe Analytics, Adobe DTM, Tealium */
+    date: 'August 2017 - November 2018',
+    role: ['Digital Analytics Development', 'A/B Test Development'],
+    tech: ['HTML', 'CSS', 'JavaScript', 'React'] /* @todo: REVIEW */
   },
   {
     title: 'John Lewis',
     url: '//www.johnlewis.com',
     logo: johnLewis,
-    role: ['A/B and multivariate testing'],
-    tech: ['HTML', 'CSS', 'JavaScript']
-  },
-  {
-    title: 'National Rail Enquiries',
-    url: '//www.nationalrail.co.uk',
-    logo: nationalRailEnquiries,
-    role: ['A/B and multivariate testing', 'Front-end development'],
-    tech: ['HTML', 'CSS', 'JavaScript']
+    date: 'August 2013 - July 2017',
+    role: ['A/B and Multivariate Test Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'] /* @todo: REVIEW */
   },
   {
     title: 'British Airways',
     url: '//www.britishairways.com/travel/home/public/en_gb',
     logo: britishAirways,
-    role: ['A/B and multivariate testing'],
-    tech: ['HTML', 'CSS', 'JavaScript']
+    date: 'August 2013 - July 2017',
+    role: ['A/B and Multivariate Test Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'] /* @todo: REVIEW */
+  },
+  {
+    title: 'National Rail Enquiries',
+    url: '//www.nationalrail.co.uk',
+    logo: nationalRailEnquiries,
+    date: 'August 2013 - July 2017',
+    role: ['A/B and Multivariate Test Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'] /* @todo: REVIEW */
   },
   {
     title: 'Ebuyer',
     url: '//www.ebuyer.com',
     logo: ebuyer,
-    role: ['A/B and multivariate testing'],
-    tech: ['HTML', 'CSS', 'JavaScript']
+    date: 'August 2013 - July 2017',
+    role: ['A/B and Multivariate Test Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'] /* @todo: REVIEW */
   },
   {
-    title: 'Eurostar',
-    url: '//www.eurostar.com',
-    logo: eurostar,
-    role: ['Analytics implementation', 'A/B and multivariate testing'],
-    tech: ['HTML', 'CSS', 'JavaScript', 'React'], /* Google Analytics, Google Tag Manager, Tealium */
-    isPrimary: true,
-    isContract: true
-  },
-  {
-    title: 'Thomas Cook',
-    url: '//www.thomascook.com',
-    logo: thomasCook,
-    role: ['A/B and multivariate testing'],
-    tech: ['HTML', 'CSS', 'JavaScript', 'Angular']
+    title: 'Energy Helpline',
+    url: '//www.energyhelpline.com',
+    logo: energyHelpline,
+    date: 'August 2013 - July 2017',
+    role: ['A/B and Multivariate Test Development'],
+    tech: ['HTML', 'CSS', 'JavaScript', 'Angular'] /* @todo: REVIEW */
   },
   {
     title: 'Heathrow Express',
     url: '//www.heathrowexpress.com',
     logo: heathrowExpress,
-    role: ['A/B and multivariate testing'],
-    tech: ['HTML', 'CSS', 'JavaScript']
+    date: 'August 2013 - July 2017',
+    role: ['A/B and Multivariate Test Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'] /* @todo: REVIEW */
   },
   {
-    title: 'Energy Helpline',
-    url: '//www.energyhelpline.com', /* Redirects to https://www.energyhelpline.com/fri/ */
-    logo: energyHelpline,
-    role: ['A/B and multivariate testing'],
-    tech: ['HTML', 'CSS', 'JavaScript', 'Angular']
-  },
-  {
-    title: 'Belsize Bathrooms',
-    url: '//www.belsizebathrooms.co.uk',
-    logo: belsizeBathrooms,
-    role: ['Design', 'Front-end development', 'Back-end development'],
-    tech: ['HTML', 'CSS', 'JavaScript', 'PHP']
+    title: 'Tealman',
+    subTitle: 'Personal Project',
+    url: '//chrome.google.com/webstore/detail/tealman/kbppiimbnpoalogphgccdikkjhjolmfn',
+    logo: tealman,
+    date: 'January 2019 - Present',
+    role: ['Chrome DevTools Extension Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'] /* @todo: REVIEW */
   },
   {
     title: 'tint',
+    subTitle: 'Personal Project',
     url: '//play.google.com/store/apps/details?id=com.temelm.tint',
     logo: tint,
-    role: ['Design', 'Hybrid mobile app development'],
-    tech: ['HTML', 'CSS', 'JavaScript'] /* Adobe PhoneGap */
+    date: 'December 2015 - August 2016',
+    role: ['Hybrid Mobile App Development'],
+    tech: ['HTML', 'CSS', 'JavaScript'] /* @todo: REVIEW */
   }
 ]
 
